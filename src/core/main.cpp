91ccb55bcc86f859de214cbf4e7cbcd001e28ffb
Carlos Aguilera <18aguilerac@gmail.com>
2022-11-11 12:39:04 -0800

    updating window size and cmakelist

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
   QWidget window;
-  window.resize(320, 240);
+  window.resize(640, 480);
   window.show();
   window.setWindowTitle(QApplication::translate("toplevel", "Top-level widget"));
   return app.exec();
