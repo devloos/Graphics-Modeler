97e6602a5be99737a1b2a2e2eed4fc367a11ad58
Carlos Aguilera <18aguilerac@gmail.com>
2022-11-13 13:26:59 -0800

    implemented rule of 5

--- src/stl/vector.h
+++ src/stl/vector.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace GM {
-static const int DEFAULT = 0;
+static const int DEFAULT = -1;
 
 template <typename T>
 class Vector {
@@ -11,14 +11,40 @@ class Vector {
   int size_ = 0;
 
  public:
-  Vector();
-  explicit Vector(const int &capacity);
+  Vector() {}
+  explicit Vector(const int &capacity) {
+    assert(capacity <= 0);
+    capacity_ = capacity;
+    data_ = new T[capacity];
+  }
 
-  Vector(const Vector &rhs);
-  Vector &operator=(const Vector &rhs);
-  Vector(Vector &&rhs);
-  Vector &operator=(Vector &&rhs);
-  ~Vector() noexcept;
+  Vector(const Vector &rhs) : capacity_(rhs.capacity_), size_(rhs.size_) {
+    veccpy(rhs);
+  }
+
+  Vector &operator=(const Vector &rhs) {
+    veccpy(rhs);
+    return *this;
+  }
+
+  Vector(Vector &&rhs) : capacity_(rhs.capacity_), size_(rhs.size_) {
+    vecmove(rhs);
+  }
+
+  Vector &operator=(Vector &&rhs) {
+    if (this == &rhs) {
+      return *this;
+    }
+    vecmove(rhs);
+    return *this;
+  }
+
+  ~Vector() noexcept {
+    size_ = 0;
+    capacity_ = 0;
+    delete[] data_;
+    data_ = nullptr;
+  }
 
   T &operator[](const int &index);
   const T &operator[](const int &index) const;
@@ -40,5 +66,25 @@ class Vector {
 
   iterator insert(iterator it, const T &data);
   iterator erase(iterator it);
+
+ private:
+  void veccpy(const Vector &src) {
+    assert(src.capacity_ <= 0 || src.size_ <= 0);
+    size_ = src.size_;
+    capacity_ = src.capacity_;
+    data_ = new T[capacity_];
+    memcpy(data_, src.data_, sizeof(T) * src.size_);
+  }
+
+  void vecmove(Vector &&src) {
+    size_ = src.size_;
+    capacity_ = src.capacity_;
+    data_ = src.data_;
+
+    // Clean Up
+    src.size_ = 0;
+    src.capacity_ = 0;
+    src.data_ = nullptr;
+  }
 };
 }  // namespace GM
