28de9cf8e7f41d8075cb56d6f7892185b3befc31
Carlos Aguilera <18aguilerac@gmail.com>
2022-11-13 15:44:58 -0800

    testing done for push back and others

--- src/gm/vector.h
+++ src/gm/vector.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <cassert>
 #include <cstring>
+#include <iostream>
+#include <vector>
 
 namespace GM {
-static const int DEFAULT = -1;
-
 template <typename T>
 class Vector {
  private:
@@ -14,6 +14,7 @@ class Vector {
 
  public:
   Vector() {}
+
   explicit Vector(const int &capacity) {
     assert(capacity > 0);
     capacity_ = capacity;
@@ -48,14 +49,62 @@ class Vector {
     data_ = nullptr;
   }
 
-  T &operator[](const int &index);
-  const T &operator[](const int &index) const;
+  T &operator[](const int &index) {
+    assert(index >= 0 && index < size_);
+    return data_[index];
+  }
 
-  int size() const;
-  int capacity() const;
+  const T &operator[](const int &index) const {
+    assert(index >= 0 && index < size_);
+    return data_[index];
+  }
 
-  void resize(const int &newSize = DEFAULT);
-  void push_back(const T &data);
+  int size() const {
+    return size_;
+  }
+  int capacity() const {
+    return capacity_;
+  }
+
+  void resize(const int &count, const T &value = T()) {
+    assert(count > 0);
+    T* block = new T[count];
+
+    if (count <= size_) {
+      deepCpy(block, data_, count);
+    } else {
+      deepCpy(block, data_, size_);
+      for (int i = size_; i < count; i++) {
+        block[i] = value;
+      }
+    }
+
+    size_ = count;
+    capacity_ = count;
+    delete[] data_;
+    data_ = block;
+    block = nullptr;
+  }
+
+  bool isEmpty() {
+    return size_ == 0;
+  }
+
+  void print() {
+    for (int i = 0; i < size_; i++) {
+      std::cout << data_[i] << " ";
+    }
+    std::cout << "\n";
+  }
+
+  void push_back(const T &value) {
+    if (size_ >= capacity_) {
+      reAlloc((capacity_ + 1) + (capacity_ / 2));
+    }
+
+    data_[size_++] = value;
+  }
+
   void reserve(const int &allocSize);
 
   typedef T* iterator;
@@ -72,10 +121,14 @@ class Vector {
  private:
   void veccpy(const Vector &src) {
     assert(src.capacity_ > 0 || src.size_ > 0);
+
     size_ = src.size_;
     capacity_ = src.capacity_;
     data_ = new T[capacity_];
-    memcpy(data_, src.data_, sizeof(T) * src.size_);
+
+    for (int i = 0; i < size_; i++) {
+      data_[i] = src.data_[i];
+    }
   }
 
   void vecmove(Vector &&src) {
@@ -87,6 +140,24 @@ class Vector {
     src.size_ = 0;
     src.capacity_ = 0;
     src.data_ = nullptr;
+  }
+
+  void deepCpy(T* dest, const T* src, const int &size) {
+    for (int i = 0; i < size; i++) {
+      dest[i] = src[i];
+    }
+  }
+
+  void reAlloc(const int &count) {
+    assert(count > capacity_);
+
+    T* block = new T[count];
+    deepCpy(block, data_, size_);
+
+    capacity_ = count;
+    delete[] data_;
+    data_ = block;
+    block = nullptr;
   }
 };
 }  // namespace GM
