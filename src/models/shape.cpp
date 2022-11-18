04969cc97e47843eee659f10ac507e247cc153f1
Catatonic2003 <eastwoodliam@gmail.com>
2022-11-17 16:50:06 -0800

    Update shape.cpp

--- src/models/shape.cpp
+++ src/models/shape.cpp
@@ -8,68 +8,68 @@ shape::shape() {
 shape::shape(
     int id, QString name, GM::Vector<QPoint> point, QColor color, int width,
     Qt::PenStyle style, Qt::PenCapStyle cap, Qt::PenJoinStyle join) {
-  shape::shapeId = id;
-  shape::shapeName = name;
-  shape::points = point;
-  shape::shapePen.setColor(color);
-  shape::shapePen.setWidth(width);
-  shape::shapePen.setStyle(style);
-  shape::shapePen.setCapStyle(cap);
-  shape::shapePen.setJoinStyle(join);
+  this->shapeId = id;
+  this->shapeName = name;
+  this->points = point;
+  this->shapePen.setColor(color);
+  this->shapePen.setWidth(width);
+  this->shapePen.setStyle(style);
+  this->shapePen.setCapStyle(cap);
+  this->shapePen.setJoinStyle(join);
 }
 
 void shape::setPenWidth(int width) {
-  shape::shapePen.setWidth(width);
+  this->shapePen.setWidth(width);
 }
 
 void shape::setPenStyle(Qt::PenStyle style) {
-  shape::shapePen.setStyle(style);
+  this->shapePen.setStyle(style);
 }
 
 void shape::setPenCap(Qt::PenCapStyle cap) {
-  shape::shapePen.setCapStyle(cap);
+  this->shapePen.setCapStyle(cap);
 }
 
 void shape::setPenColor(QColor color) {
-  shape::shapePen.setColor(color);
+  this->shapePen.setColor(color);
 }
 
 void shape::setPenJoin(Qt::PenJoinStyle joinStyle) {
-  shape::shapePen.setJoinStyle(joinStyle);
+  this->shapePen.setJoinStyle(joinStyle);
 }
 
 void shape::setPenWidth(int width) {
-  shape::shapePen.setWidth(width);
+  this->shapePen.setWidth(width);
 }
 
 void shape::pointPush_Back(QPoint newPoint) {
-  shape::points.push_back(newPoint);
+  this->points.push_back(newPoint);
 }
 
 void shape::pointPop_Back() {
-  shape::points.pop_back();
+  this->points.pop_back();
 }
 
 void shape::setShapeName(QString name) {
-  shape::shapeName = name;
+  this->shapeName = name;
 }
 
 void shape::setShapeId(int id) {
-  shape::shapeId = id;
+  this->shapeId = id;
 }
 
 QPoint &shape::getPointAt(int x) {
-  return shape::points[x];
+  return this->points[x];
 }
 
 GM::Vector<QPoint> &shape::getPoints() {
-  return shape::points;
+  return this->points;
 }
 
 int shape::getShapeId() {
-  return shape::shapeId;
+  return this->shapeId;
 }
 
 QString shape::getShapeName() {
-  return shape::shapeName;
+  return this->shapeName;
 }
