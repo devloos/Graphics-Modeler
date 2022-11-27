e5d5c52b77368e0fc8a16fa483331cf2a627ca87
Cameron Rosenthal <supernovawarriors128@gmail.com>
2022-11-26 20:22:54 -0800

    Added text-specific members

--- src/models/text_shape.cpp
+++ src/models/text_shape.cpp
@@ -1,6 +1,7 @@
 #include "text_shape.h"
 
-Text::Text()
+Text::Text(QString str)
+    :textStr(str)
 {
     
 }
@@ -29,3 +30,23 @@ void Text::move()
 {
 
 }
+
+
+// More text-specific --------------------------------------
+
+// Setters
+void Text::setText(QString str) { textStr = str; }
+void Text::setTextAlignment(Qt::AlignmentFlag alignment) { textAlignment = alignment; }
+void Text::setFontSize(int size) { fontSize = size; }
+void Text::setFontFamily(std::string family) { fontFamily = family; }
+void Text::setFontStyle(QFont::Style style) { fontStyle =  style; }
+void Text::setFontWeight(QFont::Weight weight) { fontWeight = weight; }
+
+// Getters
+QString Text::getText() const { return textStr; }
+Qt::AlignmentFlag Text::getTextAlignment() const { return textAlignment; }
+int Text::getFontSize() const { return fontSize; }
+std::string Text::getFontFamily() const { return fontFamily; }
+QFont::Style Text::getFontStyle() const { return fontStyle; }
+QFont::Weight Text::getFontWeight() const { return fontWeight; }
+
