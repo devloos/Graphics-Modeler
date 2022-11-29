93391c6a36636ccc36290c5bfd64b6704d01c7ae
Carlos Aguilera <18aguilerac@gmail.com>
2022-11-29 12:59:00 -0800

    push

--- src/qt/widgets/main.cpp
+++ src/qt/widgets/main.cpp
@@ -1,5 +1,15 @@
 #include "main.h"
 
-MainWidget::MainWidget() {}
+MainWidget::MainWidget() {
+  setAutoFillBackground(true);
+  setPalette(QPalette(QColor(34, 50, 77)));
+
+  QLabel* label = new QLabel(this);
+
+  label->setGeometry(
+      (GM::Window::WIDTH / 2) - 160, (GM::Window::HEIGHT / 2) - 25, 350, 50);
+  label->setText("IN PROGRESS");
+  label->setStyleSheet("font-size: 50px");
+}
 
 MainWidget::~MainWidget() noexcept {}
