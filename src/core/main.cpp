ae718c7fcd4c58741d00d2782682de33846c8129
Carlos Aguilera <18aguilerac@gmail.com>
2022-12-08 14:50:20 -0800

    pop up centering

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]) {
   QGuiApplication app(argc, argv);
 
   QQmlApplicationEngine engine;
-  const QUrl url(u"qrc:/Window.qml"_qs);
+  const QUrl url(u"qrc:/App.qml"_qs);
 
   QObject::connect(
       &engine, &QQmlApplicationEngine::objectCreated, &app,
