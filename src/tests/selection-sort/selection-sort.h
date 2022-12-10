c22355d5307470383be35f1d0e9a57ef3237b428
Connor Darling <connordarling@me.com>
2022-12-09 18:47:11 -0800

    selection sort implemented

--- src/tests/selection-sort/selection-sort.h
+++ src/tests/selection-sort/selection-sort.h
@@ -1,13 +1,48 @@
 #pragma once
-#include "gm/vector.h"
 
+#include "gm/vector.h"
+#include "models/free-form/circle.h"
+
+template <typename T>
+bool compare_shape_perimeter(
+    typename GM::Vector<T>::iterator &it, typename GM::Vector<T>::iterator &minIt) {
+  return (*it).perimeter() < (*minIt).perimeter();
+}
+
+template <typename T>
+bool compare_shape_area(
+    typename GM::Vector<T>::iterator &it, typename GM::Vector<T>::iterator &minIt) {
+  return (*it).area() < (*minIt).area();
+}
+
+// sorts by function pointer
+template <typename T>
+void selection_sort(
+    GM::Vector<T> &vec,
+    bool (*compare)(
+        typename GM::Vector<T>::iterator &, typename GM::Vector<T>::iterator &)) {
+  using it = typename GM::Vector<T>::iterator;
+  for (it itor = vec.begin(); itor != vec.end(); ++itor) {
+    it minItor = itor;
+    for (it i = itor + 1; i != vec.end(); ++i) {
+      if (compare(i, minItor)) {
+        minItor = i;
+      }
+    }
+    if (minItor != itor) {
+      std::iter_swap(itor, minItor);
+    }
+  }
+}
+
+// Sorts by shape id
 template <typename T>
 void selection_sort(GM::Vector<T> &vec) {
   using it = typename GM::Vector<T>::iterator;
   for (it itor = vec.begin(); itor != vec.end(); ++itor) {
     it minItor = itor;
     for (it i = itor + 1; i != vec.end(); ++i) {
-      if (*i < *minItor) {
+      if ((*i).getShapeId() < (*minItor).getShapeId()) {
         minItor = i;
       }
     }
@@ -18,5 +53,15 @@ void selection_sort(GM::Vector<T> &vec) {
 }
 
 namespace Test {
-void ints();  //
+
+template <typename T>
+void print(const GM::Vector<T> &vec) {
+  for (auto itor = vec.begin(); itor != vec.end(); itor++) {
+    std::cout << *itor << " ";
+  }
+  std::cout << '\n';
+}
+
+void sortingShapeByPer();
+void sortingShapeById();
 }  // namespace Test
--- src/tests/selection-sort/sort-test.cpp
+++ src/tests/selection-sort/sort-test.cpp
@@ -1,27 +1,50 @@
 #include "selection-sort.h"
 
 namespace Test {
-template <typename T>
-void print(const GM::Vector<T> &vec) {
-  for (auto itor = vec.begin(); itor != vec.end(); itor++) {
-    std::cout << *itor << " ";
-  }
-  std::cout << '\n';
-}
 
-void ints() {
-  GM::Vector<int> test;
-  test.push_back(1);
-  test.push_back(2);
-  test.push_back(501);
-  test.push_back(69);
-  test.push_back(48);
-  test.push_back(153629);
-  test.push_back(6666);
-  std::cout << "Unsorted Vector: ";
-  print(test);
-  selection_sort(test);
-  std::cout << "Sorted Vector: ";
-  print(test);
-}
+// Tests that require shape hierarchy to be implemented
+
+// void sortingShapeByPer() {
+//   std::cout << "[Sorting by shape perimeter]\n";
+//   GM::Vector<Circle> test;
+//   Circle one(2.5);
+//   std::cout << one.perimeter() << "\n";
+//   Circle two(3.14);
+//   std::cout << two.perimeter() << "\n";
+//   Circle three(1.0);
+//   std::cout << three.perimeter() << "\n";
+//   test.push_back(one);
+//   test.push_back(two);
+//   test.push_back(three);
+//   std::cout << "\n"
+//             << "Unsorted Vector : ";
+//   print(test);
+//   selection_sort<Circle>(test, compare_shape_perimeter<Circle>);
+//   std::cout << "Sorted Vector   : ";
+//   print(test);
+// }
+
+// void sortingShapeById() {
+//   std::cout << "[Sorting by shape id]\n";
+//   GM::Vector<Circle> test;
+//   Circle one;
+//   one.setShapeId(1300);
+//   std::cout << one.getShapeId() << "\n";
+//   Circle two;
+//   two.setShapeId(-1);
+//   std::cout << two.getShapeId() << "\n";
+//   Circle three;
+//   three.setShapeId(1250);
+//   std::cout << three.getShapeId() << "\n";
+//   test.push_back(one);
+//   test.push_back(two);
+//   test.push_back(three);
+//   std::cout << "\n"
+//             << "Unsorted Vector : ";
+//   print(test);
+//   selection_sort<Circle>(test);
+//   std::cout << "Sorted Vector   : ";
+//   print(test);
+// }
+
 }  // namespace Test
