488628aab7e7fe896d2abe55f09117317c02bac7
Carlos Aguilera <18aguilerac@gmail.com>
2022-12-10 20:57:22 -0800

    polyline paint done

--- src/models/polygon/polyline.cpp
+++ src/models/polygon/polyline.cpp
@@ -14,4 +14,14 @@ float Polyline::perimeter() const {
 
 void Polyline::paint(QPainter* painter) {
   std::cout << "Polyline\n";
+  Polyline* polyline = (Polyline*)(shapes.at(1).get());
+
+  painter->setPen(polyline->pen_);
+
+  QList<QPoint> list;
+  for (const auto &point : polyline->points_) {
+    list.push_back(point);
+  }
+
+  painter->drawPolyline(QPolygon(list));
 }
